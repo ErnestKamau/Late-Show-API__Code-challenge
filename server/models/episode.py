41ef4f94ac9ca2261ca914179bc6ea9e3a1dd427
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-22 07:18:01 +0300

    Add Episode model

---
+++ server/models/episode.py
@@ -0,0 +1,16 @@
+from sqlalchemy_serializer import SerializerMixin
+from config import db
+
+
+class Episode(db.Model, SerializerMixin):
+    __tablename__ = 'episodes'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+    
+    appearances = db.relationship("Appearance", backref="episode", cascade="all, delete")
+    
+    def __repr__(self):
+        return f"<Episode #{self.number} on {self.date}>"
+    
